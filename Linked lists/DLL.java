939706003a535c4d2e337f566bfb08fad031694b
Vaibhav Katyal <vaibhav0480.becse24@chitkara.edu.in>
2025-11-20 05:17:23 +0530

    LL

---
+++ Linked lists/DLL.java
@@ -0,0 +1,21 @@
+class NodeDLL{
+    int data;
+    NodeDLL next;
+    NodeDLL prev;
+
+    NodeDLL(int val){
+        this.data = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList{
+    
+}
+
+public class DLL{
+    public static void main(String[] args) {
+        
+    }
+}
